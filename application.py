083b56f818ba1780d0cd3e817ca9759c348f8ba3
Darren Jonany <61177490+darrenjonany@users.noreply.github.com>
2022-12-07 10:16:15 +0700

    updated application.py

--- application.py
+++ application.py
@@ -2,6 +2,6 @@ print("This is my python application program")
 
 var = 10
 color = "blue"
+name = "John Doe"
 
-print("my fav number" , str(var))
-print("my fav color" , color)
+print(f"Hello my name is {name}, my fav number" , str(var), f" my favorite color is {color}")
