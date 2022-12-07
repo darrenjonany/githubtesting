1b9a604e36a633d714a46f4a48d1708dc283505f
Darren Jonany <61177490+darrenjonany@users.noreply.github.com>
2022-12-07 10:13:24 +0700

    update applciation.py to add name info

--- application.py
+++ application.py
@@ -2,6 +2,8 @@ print("This is my python application program")
 
 var = 10
 color = "blue"
+name = "John Doe"
 
+print(f"Hello my name is {name}")
 print("my fav number" , str(var))
 print("my fav color" , color)
